2cf3c7f9d1d4dee840579250d217d3cc95ac3ed7
André <andrefm06@gmail.com>
2026-07-06 20:02:39 -0300

    Add class Main.

---
+++ lab_ordenacao/src/lab_ordenacao/Main.java
@@ -0,0 +1,16 @@
+package lab_ordenacao;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("André",20,"Azul");
+        Pessoa p2 = new Pessoa("João",23,"Vermelho");
+        Pessoa p3 = new Pessoa("Lucas",10,"Amarelo");
+        Pessoa p4 = new Pessoa("Carlos",17,"Verde");
+        Pessoa p5 = new Pessoa("Beatriz",23,"Vermelho");
+        Pessoa[] pessoas = {p1,p2,p3,p4,p5};
+
+        Ordenacao ordenacao = new Ordenacao();
+        //ordenacao.bubbleSort(pessoas);
+        ordenacao.selectionSort(pessoas);
+    }
+}
