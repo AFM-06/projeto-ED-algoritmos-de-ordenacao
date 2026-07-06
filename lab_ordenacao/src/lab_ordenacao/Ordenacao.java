62d5e9848c243a8ae5bedc0de7876edd8d82f83e
André <andrefm06@gmail.com>
2026-07-06 20:01:17 -0300

    Add right method to print the array pessoas[].

--- lab_ordenacao/src/lab_ordenacao/Ordenacao.java
+++ lab_ordenacao/src/lab_ordenacao/Ordenacao.java
@@ -17,7 +17,7 @@ public class Ordenacao implements Ordenacao_IF{
 
 	@Override
 	public void bubbleSort(Pessoa[] pessoas) {
-		System.out.println(pessoas);
+		System.out.println(Arrays.toString(pessoas));
 		int size = pessoas.length;
 		for(int i = 0; i < size-1; i++) {
 			for(int j = 0; j < size - 1 - i;j++){
